38cc6dabee2a150824264ce912230afda72b8fb7
AHN MIYOUNG <imamy98@gmail.com>
2012-05-17 19:34:22 +0900

    second java file created
    Change-Id: I5f908b990397d335977914f7719a3f8a02ccf9af
    Signed-off-by: AHN MIYOUNG <imamy98@gmail.com>

---
+++ Sample/src/nexcore/Sample2.java
@@ -0,0 +1,12 @@
+package nexcore;
+
+public class Sample2 {
+
+	public void op_02(String param){
+		System.out.println("a");
+	}
+	
+	public void op_03(String param){
+		System.out.println("a");
+	}
+}
